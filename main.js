4724f9688683e537c8f8086276a0ef0895476224
yuranw3 <42990390+yuranw3@users.noreply.github.com>
2019-05-31 15:26:59 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,73 +1,51 @@
 import Vue from 'vue'
 
-import 'normalize.css/normalize.css' // A modern alternative to CSS resets
+import 'normalize.css/normalize.css'// A modern alternative to CSS resets
+
+import 'babel-polyfill'
+
+{{#if isElement}}
 import ElementUI from 'element-ui'
-import '@/assets/theme/index.css'
+import 'element-ui/lib/theme-chalk/index.css'
+import locale from 'element-ui/lib/locale/lang/zh-CN'
+Vue.use(ElementUI, { locale })
+{{/if}}
+
 import '@/styles/index.scss' // global css
+
 import App from './App'
 import router from './router'
 import store from './store'
+
 import '@/icons' // icon
 import '@/assets/iconfont/iconfont.css'
 import '@/permission' // permission control
-// openapi调用，访问地址是网关
-import OPENAPIrequest from '@/utils/APIRequest'
 
-import { getParams } from '@/api/openapi'
-// 非openapi调用
-import request from '@/utils/request'
-import API from '@/api/api'
+{{#if multi}}
+const multi = true
+{{/if}}
 {{#if stor}}
-// 青云oss
 import $QStor from '@/utils/QingStor'
 window.$QStor = $QStor
+window.$QStor.storConfig = {
+   host: {{storConfig.host}},
+   port: {{storConfig.port}},
+   protocol: {{storConfig.protocol}},
+   access_key_id: {{storConfig.access_key_id}},
+   secret_access_key: {{storConfig.secret_access_key}},
+   bucket: {{storConfig.bucket}},
+   zone: {{storConfig.zone}}
+}
 {{/if}}
-import './filters'
-import './mixins'
-// import 'element-ui/lib/theme-chalk/index.css'
-// import locale from 'element-ui/lib/locale/lang/zh-CN' // lang i18n
-// import 'babel-polyfill'
-Vue.use(ElementUI)
+{{#if openapi}}
+const openAPI = true
+{{/if}}
+{{#if editor}}
+const editor = true
+{{/if}}
+
 
 Vue.config.productionTip = false
-
-function APIRequest(apiId, data) {
-  const apiParams = getParams(apiId)
-  if (apiParams) {
-    const requestData = {
-      data: data,
-      id: apiId
-    }
-    return OPENAPIrequest(requestData)
-  }
-}
-
-window.$httpRequest = APIRequest
-
-function http(url, method, data) {
-  const params = {
-    url, method,
-    data
-  }
-  return request(params)
-}
-
-window.$HTTP = http
-
-function httpRequest(api, method, data) {
-  const apiParams = API[api]
-  if (apiParams) {
-    const requestData = {
-      data: data,
-      id: api
-    }
-    return OPENAPIrequest(requestData)
-  }
-}
-
-window.$httpRequest = httpRequest
-
-
 
 new Vue({
   el: '#app',
